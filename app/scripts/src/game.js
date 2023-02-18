e73d88774fe92c89abc49da145d4ea1d1cfc8cdf
Fokymaki <nshepelov@mail.ru>
2023-02-18 17:15:33 +0300

    Фаст апдейт

--- app/scripts/src/game.js
+++ app/scripts/src/game.js
@@ -123,8 +123,8 @@ let zone_endpoint = {index: -1, cell: 0}
 var gunshot_audio = new Audio('sounds/gunshot.mp3');
 var sword_audio = new Audio('sounds/sword.wav');
 
-gunshot_audio.volume = 0.3
-sword_audio.volume = 0.3
+gunshot_audio.volume = 0.2
+sword_audio.volume = 0.2
 
 function reconnect() {
   if (!socket.isReady()) {
