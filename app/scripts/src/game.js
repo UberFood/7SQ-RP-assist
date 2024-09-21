e3d027d7fc3e36c1efa50fcbb4bce44c8f61eb4d
UberFood <59709698+UberFood@users.noreply.github.com>
2024-09-21 19:32:32 +0600

    Add constants back for quick fix

--- app/scripts/src/game.js
+++ app/scripts/src/game.js
@@ -66,6 +66,201 @@ var DECREASE_AGILITY = 7;
 var DECREASE_INT = 8;
 var DECREASE_KD = 9;
 
+const CHARACTER_INFO_CONTANER_SELECTOR = '[data-name="character-info-container"]';
+const WEAPON_INFO_CONTANER_SELECTOR = '[data-name="weapon-info-container"]';
+const NOTIFICATIONS_CONTANER_SELECTOR = '[data-name="notifications-container"]';
+const INITIATIVE_ORDER_CONTANER_SELECTOR = '[data-name="initiative-order-display-container"]';
+const INITIATIVE_DROPBOX_CONTAINER_SELECTOR = '[data-name="initiative-order-dropbox-container"]';
+
+const SHOW_BOARD_CREATION_GROUP_BUTTON_SELECTOR = '[data-name="show_board_creation_group_button"]';
+const SHOW_BOARD_EDIT_GROUP_BUTTON_SELECTOR = '[data-name="show_board_edit_group_button"]';
+const SHOW_BATTLE_CONTROL_GROUP_BUTTON_SELECTOR = '[data-name="show_battle_control_group_button"]';
+const CREATE_BOARD_BUTTON_SELECTOR = '[data-name="create_board_button"]';
+const SAVE_BOARD_BUTTON_SELECTOR = '[data-name="save_board_button"]';
+const LOAD_BOARD_BUTTON_SELECTOR = '[data-name="load_board_button"]';
+const ROLL_INITIATIVE_BUTTON_SELECTOR = '[data-name="roll_initiative_button"]';
+const RESET_INITIATIVE_BUTTON_SELECTOR = '[data-name="reset_initiative_button"]';
+const FOG_BUTTON_SELECTOR = '[data-name="fog_button"]';
+const ZONE_BUTTON_SELECTOR = '[data-name="zone_button"]';
+const CHAT_BUTTON_SELECTOR = '[data-name="chat_button"]';
+const MIRROR_BUTTON_SELECTOR = '[data-name="mirror_button"]';
+const NEXT_ROUND_BUTTON_SELECTOR = '[data-name="next_round_button"]';
+const BATTLE_MOD_BUTTON_SELECTOR = '[data-name="battle_mod_button"]';
+const SYNC_BUTTON_SELECTOR = '[data-name="sync_button"]';
+const LANDMINE_BUTTON_SELECTOR = '[data-name="landmine_button"]';
+const FOG_ZONE_BUTTON_SELECTOR = '[data-name="fog_zone_button"]';
+const UNFOG_ZONE_BUTTON_SELECTOR = '[data-name="unfog_zone_button"]';
+const DOWNLOAD_BOARD_BUTTON_SELECTOR = '[data-name="download_board_button"]';
+
+const SAVES_SELECT_SELECTOR = '[data-name="saves_select"]';
+
+const ZONE_NUMBER_SELECTOR = '[data-name="zone_number_select"]';
+const SEARCH_MODIFICATOR_SELECTOR = '[data-name="search_modificator"]';
+
+const BOARD_SIZE_INPUT_SELECTOR = '[data-name="board_size_input"]';
+const SAVE_NAME_INPUT_SELECTOR = '[data-name="save_name_input"]';
+
+const NOTIFICATIONS_LIST_SELECTOR = '[data-name="notifications_list"]';
+
+const SKILL_MODAL_SELECTOR = '[data-name="skill-modal"]';
+const SKILL_MODAL_CONTENT_SELECTOR = '[data-name="skill-modal-content"]';
+const SKILL_DESCRIPTION_CONTAINER_SELECTOR = '[data-name="skill-description-container"]';
+const NEXT_PAGE_BUTTON_CONTAINER_SELECTOR = '[data-name="next-page-button-container"]';
+
+const BOARD_CREATION_GROUP_SELECTOR = '[data-group-name="board_creation_group"]';
+const BOARD_EDIT_GROUP_SELECTOR = '[data-group-name="board_edit_group"]';
+const BATTLE_CONTROL_GROUP_SELECTOR = '[data-group-name="battle_control_group"]';
+
+const stamina_weakspot_cost = 1
+const stamina_attack_cost = 1
+const punch_rainfall_stamina_cost = 2 // per punch
+const stamina_cut_limb_cost = 4
+const shield_up_stamina_cost = 2 // per move I think
+const lottery_shot_stamina_cost = 1
+const lucky_shot_stamina_cost = 1
+const action_splash_stamina_cost = 2
+export const adrenaline_stamina_cost = 3
+export const acid_bomb_stamina_cost = 3
+export const curved_bullets_stamina_cost = 2
+
+export const cut_limb_cooldown = 1
+
+export const rest_stamina_gain = 5
+
+export const cut_limb_duration = 1
+export const cooldown_big_bro = 1 // duration
+
+export const shield_up_KD = 3
+
+export const adrenaline_move_increase = 4
+
+export const sniper_passive_penalty = -5;
+
+export const gas_bomb_base_threshold = 10
+export const gas_bomb_move_reduction = 2
+export const gas_bomb_stamina_cost = 3
+export const gas_bomb_obstacle = 15
+export const gas_bomb_skill_cooldown = 5
+
+export const light_sound_bomb_radius = 4
+export const light_sound_bomb_threshold = 15
+export const light_sound_bomb_skill_cooldown = 5
+
+export const weak_spot_threshold = 15;
+export const weak_spot_range = 25;
+export const weak_spot_cover_impossible_threshold = 10;
+
+export const shocked_cooldown = 0
+
+export const pich_pich_cooldown = 4
+export const pich_pich_move_increase = 4
+
+export const force_field_radius = 1.6
+export const force_field_stamina_cost = 5
+export const force_field_cooldown = 10
+export const force_field_obstacle = 24
+
+export const action_splash_cooldown = 4
+
+export const invisibility_detection_radius = 2;
+export const invisibility_detection_threshold = 10;
+
+export const big_bro_range = 2
+export const heal_range = 1
+export const throw_base_range = 2
+export const light_sound_bomb_range = 5
+export const force_field_range = 1
+export const adrenaline_range = 1
+export const poisonous_adrenaline_range = 1
+
+export const poisonous_adrenaline_duration = 2
+export const poisonous_adrenaline_cooldown = 5
+export const poisonous_adrenaline_flat_HP = 5
+export const poisonous_adrenaline_flat_stamina = 5
+export const poisonous_adrenaline_percent_HP = 0.05
+export const poisonous_adrenaline_percent_stamina = 0.05
+
+export const adrenaline_cooldown = 4
+
+export const acid_bomb_duration = 2 // 2+1 really
+export const acid_bomb_cooldown = 5
+export const acid_bomb_radius = 1.6
+
+export const mines_1_distance_damage = 350
+export const landmine_detection_radius = 4;
+export const landmine_diffusion_radius = 3;
+export const landmine_diffuse_threshold = 10
+
+export const tobacco_strike_hp_percentage = 0.1
+export const tobacco_strike_bonus = 4
+export const tobacco_strike_cooldown = 1
+
+export const safety_service_range = 2;
+export const safety_service_cooldown = 1;
+export const safety_service_duration = 1;
+export const safety_service_defensive_advantage = 1;
+export const safety_service_evade_bonus = 3;
+export const safety_service_bonus_actions_cost = 2;
+
+export const calingalator_range = 1.6;
+export const calingalator_obstacle = 25;
+export const calingalator_duration = 3;
+export const calingalator_stamina_cost = 3;
+export const calingalator_radius = 1.6;
+export const calingalator_skill_cooldown = 10;
+export const calingalator_flat_heal = 5;
+export const calingalator_roll_heal = 5;
+export const calingalator_poisoning_duration_stage1 = 5;
+export const calingalator_poisoning_duration_stage2 = 7;
+export const calingalator_poisoning_duration_stage3 = 10;
+export const calingalator_poisoning_duration_enlightened = 10;
+export const calingalator_penalty_stage1 = -1;
+export const calingalator_penalty_stage2 = -2;
+export const calingalator_penalty_stage3 = -3;
+export const calingalator_penalty_enlightened = 3;
+
+export const belvet_buff_range = 1.6;
+export const belvet_buff_skill_duration = 1;
+export const belvet_buff_attack_bonus = 2;
+export const belvet_buff_melee_advantage = 1;
+export const belvet_buff_ranged_advantage = 1;
+
+export const hook_cooldown = 2;
+export const hook_duration = 1;
+export const hook_defensive_advantage = -1;
+export const hook_range = 3;
+
+export const punishing_strike_cooldown = 3;
+export const punishing_strike_multiplyer = 0.5;
+
+export const computer_interaction_radius = 1;
+export const hacking_critical_fail_threshold = 10;
+export const hacking_success_threshold = 16;
+export const hacking_critical_success_threshold = 25;
+
+export const container_interaction_radius = 1.6;
+export const character_interaction_radius = 1.6;
+export const potion_making_radius = 1;
+
+
+export const jump_cover_impossible_threshold = 10;
+export const jump_base_distance = 1;
+
+export const belvet_jump_base_distance = 2;
+export const belvet_jump_weapon_id = 30;
+export const belvet_jump_cooldown = 2;
+
+export const carry_range = 1;
+export const carry_distance_modifier = 2;
+export const carry_bonus_actions_cost = 1;
+
+export const heal_potion_small_flat_hp = 10;
+export const heal_potion_small_index = 0;
+
+export const heal_potion_large_percent_hp = 0.5;
+export const heal_potion_large_index = 1;
+
+
 
 var CHARACTER_STATE_CONSTANT = {HP: [], main_action: [], bonus_action: [], move_action: [], stamina: [], initiative: [], can_evade: [], has_moved: [],
   KD_points: [], current_weapon: [], visibility: [], invisibility: [], attack_bonus: [], damage_bonus: [], universal_bonus: [], bonus_KD: [],
